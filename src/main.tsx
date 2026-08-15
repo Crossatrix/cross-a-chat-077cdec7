587f90e65c962cbaf0d758464b9e88ec76e2325b
Crossatrix <julian.grohmann6@gmail.com>
2026-08-15 10:01:20 +0200

    Delete src directory

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-// OneSignal handles service worker and notification permissions via the SDK loaded in index.html
-
-createRoot(document.getElementById("root")!).render(<App />);
